56a04658d10f860684ae86d187687749dd17ba17
Matthew Staples <staplma2@uw.edu>
2017-06-29 16:33:36 -0700

    made comments on aboutController, repo.js, adminView, and repoView

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -6,6 +6,7 @@ var app = app || {};
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
   // (put your response in a comment here)
+  // This method shows the #about section and hides all other sections. Then it calls the requestRepos method from repo.js with the method repoView.index (from repoView.js) to display the repo data. It is called in routes.js.
   aboutController.index = () => {
     $('#about').show().siblings().hide();
     app.repos.requestRepos(app.repoView.index);
--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -6,6 +6,7 @@ var app = app || {};
   repos.all = [];
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // This gets all the repo data from the Github api associated with the user specified by the GITHUB_TOKEN and sets the repos.all array to the array of this data. It is called in aboutController.js
   repos.requestRepos = function(callback) {
     $.get('/github/user/repos')
     .then(data => repos.all = data, err => console.error(err))
