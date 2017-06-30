25453d166abd0fd4b4da99c8e9b17d3e6441fbcf
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-06-30 10:46:06 -0700

    finished comments on article.js

--- starter-code/public/scripts/models/article.js
+++ starter-code/public/scripts/models/article.js
@@ -14,12 +14,15 @@ var app = app || {};
   // REVIEW: With ES6 arrow functions, if the function only has one parameter, you don't need parentheses.
   //         This is similar to saying Article.loadAll = function(rows).
     // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+
+    //the loadAll function lives within Article.js. It has a single argument, rows. Based on the sort method called we can determine that rows is an array. And based on the property calls within sort we can determine that the contents of rows our objects with a property of published on, with values that are dates. the sort method sorts them in descending order of their publishing date. The sorted row is then mapped to a new array with each object being passed as a parameter for an Article constructor. The Artile property all, which is an empty array, is then assigned to this new mapped array of Article objects.
   Article.loadAll = rows => {
     rows.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
     Article.all = rows.map(ele => new Article(ele));
   };
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  //fetchall is a method for the Article object which lives in article.js. It takes a single arguement, callback. it sends a get request with the path of /article. It then takes the response and passes it as a paramter for a callback function in a then method. The arameter is passed to the Article.loadAll method. Finally, the callback function is fired
   Article.fetchAll = callback => {
     $.get('/articles')
     .then(
@@ -27,13 +30,13 @@ var app = app || {};
         Article.loadAll(results);
         callback();
       }
-    )
+    );
   };
 
   // REVIEW: We have a new method to query our DB for a specific record, based on varying criteria
   Article.findWhere = function(field, value, callback) {
     $.get('/articles/find', {field: field, val: value})
-    .then(callback)
+    .then(callback);
   };
 
   // REVIEW: A new method for gathering all of the categories
@@ -55,6 +58,7 @@ var app = app || {};
   };
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  //numWordsByAuthor lives within article.js. It is a method for the Article object. The function calls Article.allAuthors which returns an array. The array is mapped through and each value is replaced by an object containing the author name as a vale. All the articles are filtered through to find ones that match the authors name. REgex does some crazy s*** and returns the length of words in each body. Reduce totals the words in each arguement.
   Article.numWordsByAuthor = () => {
     return Article.allAuthors().map(author => {
       return {
@@ -62,8 +66,8 @@ var app = app || {};
         numWords: Article.all.filter(a => a.author === author)
                              .map(a => a.body.match(/\b\w+/g).length)
                              .reduce((a, b) => a + b)
-      }
-    })
+      };
+    });
   };
 
   Article.stats = () => {
@@ -75,6 +79,7 @@ var app = app || {};
   };
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  //truncateTable lives within article.js. It fires an ajax reques with a request body containing the url /articls and a method of DELETE. it then console.logs and fires a callback.
   Article.truncateTable = callback => {
     $.ajax({
       url: '/articles',
